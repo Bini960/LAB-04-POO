15239cfeae493f49f2c25a826ff9b7bac7f52b4f
DarkFox90 <carlospozuelos3@gmail.com>
2025-11-12 21:21:08 -0600

    finalizadas clases modelo

--- src/main/java/modelo/usuarios/Admin.java
+++ src/main/java/modelo/usuarios/Admin.java
@@ -1,5 +1,14 @@
 package modelo.usuarios;
 
-public class Admin {
+public class Admin extends Usuario {
+
+    public Admin(String nombre, String correo, String contrasena) {
+        super(nombre, correo, contrasena);
+    }
+
+    @Override
+    public boolean esAdmin() {
+        return true;
+    }
     
 }
--- src/main/java/modelo/usuarios/Editor.java
+++ src/main/java/modelo/usuarios/Editor.java
@@ -1,5 +1,14 @@
 package modelo.usuarios;
 
-public class Editor {
+public class Editor extends Usuario {
+
+    public Editor(String nombre, String correo, String contrasena) {
+        super(nombre, correo, contrasena);
+    }
+
+    @Override
+    public boolean esAdmin() {
+        return false;
+    }
     
 }
